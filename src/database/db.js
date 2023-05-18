b8104700163856c07d54d04606cad8e1ff3800da
David Tealdi <tealdidavid12@gmail.com>
2023-05-18 19:02:19 -0300

    'version_1.0.0_estable-Config_deploy'

--- src/database/db.js
+++ src/database/db.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config();
 
-const dbMongo = process.env.DB_MONGODB || process.env.DB_MONGODB_LOCAL
+// const dbMongo = process.env.DB_MONGODB || process.env.DB_MONGODB_LOCAL
+const dbMongoDeploy = process.env.DB_MONGODB || process.env.DB_MONGODB
+
+// const connect = async () => {
+
+//     try {
+//         mongoose.set('strictQuery', false)
+//         mongoose.connect(dbMongo) 
+//         console.log('Mongo connected')
+//     } catch(error) {
+//         console.log(error)
+//         process.exit()
+//     }
+// }
 
 const connect = async () => {
 
     try {
         mongoose.set('strictQuery', false)
-        mongoose.connect(dbMongo) 
+        mongoose.connect(dbMongoDeploy) 
         console.log('Mongo connected')
     } catch(error) {
         console.log(error)
         process.exit()
     }
-} 
+}
 
 
 module.exports =  {
