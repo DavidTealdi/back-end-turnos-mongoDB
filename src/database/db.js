dcb417a145048f31dd7d4c9b5c4f16100d815496
David Tealdi <tealdidavid12@gmail.com>
2023-05-20 14:14:09 -0300

    version 1.0.0 estable. Se acomodo el archivo .env y db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,33 +1,19 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config();
 
-// const dbMongo = process.env.DB_MONGODB || process.env.DB_MONGODB_LOCAL
-const dbMongoDeploy = process.env.DB_MONGODB || process.env.DB_MONGODB
-
-// const connect = async () => {
-
-//     try {
-//         mongoose.set('strictQuery', false)
-//         mongoose.connect(dbMongo) 
-//         console.log('Mongo connected')
-//     } catch(error) {
-//         console.log(error)
-//         process.exit()
-//     }
-// }
+const dbMongo = process.env.DB_MONGODB || process.env.DB_MONGODB_LOCAL
 
 const connect = async () => {
 
     try {
         mongoose.set('strictQuery', false)
-        mongoose.connect(dbMongoDeploy) 
+        mongoose.connect(dbMongo) 
         console.log('Mongo connected')
     } catch(error) {
         console.log(error)
         process.exit()
     }
 }
-
 
 module.exports =  {
     connect
